4443652c35425c434107e437eda59b9bf076c022
rmadhn <ahmadrmdh93@gmail.com>
2024-07-27 00:33:46 +0700

    perbaikan

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function onListening() {
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
